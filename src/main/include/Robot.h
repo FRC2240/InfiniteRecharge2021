32907938f817b8fef86fa5738ba300f71c95358e
Erik Darzins <e.darzins@f5.com>
2021-03-10 13:59:04 -0700

    Ported shooter code from last season

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -6,13 +6,18 @@
 #include <frc/TimedRobot.h>
 #include <frc/Joystick.h>
 #include <frc/drive/DifferentialDrive.h>
-#include "rev/CANSparkMax.h"
+#include <frc/Compressor.h>
+#include <frc/DoubleSolenoid.h>
 #include <frc/smartdashboard/SendableChooser.h>
-#include <frc/WPILib.h>
 #include <frc/Filesystem.h>
 #include <frc/trajectory/TrajectoryUtil.h>
+
 #include <wpi/Path.h>
 #include <wpi/SmallString.h>
+#include <networktables/NetworkTable.h>
+#include <networktables/NetworkTableInstance.h>
+
+#include "rev/CANSparkMax.h"
 
 class Robot : public frc::TimedRobot {
  public:
@@ -26,12 +31,20 @@ class Robot : public frc::TimedRobot {
   void DisabledPeriodic() override;
   void TestInit() override;
   void TestPeriodic() override;
- 
+
+  bool LimelightTracking();
+  void InitializePIDControllers();
+  double CalculateRPM(double d);
+
  private:
   frc::SendableChooser<std::string> m_chooser;
   const std::string kAutoNameDefault = "Default";
   const std::string kAutoNameCustom = "My Auto";
   std::string m_autoSelected;
+
+  double constantLimelightAngle = 17;      // degrees (old 22)
+  double heightLimelight        = 23.0;    // inches
+  double heightOfTarget         = 89.75;   // inches
 
   static const int intakeMotorDeviceID         =  1;
   static const int hopperMotorDeviceID         =  2;
@@ -47,7 +60,7 @@ class Robot : public frc::TimedRobot {
   static const int frontleftMotorDeviceID      = 12;
   static const int backleftMotorDeviceID       = 13;
 
-//drivebase motors (looking from the front)//
+  //drivebase motors (looking from the front)//
   rev::CANSparkMax m_backrightMotor{backrightMotorDeviceID, rev::CANSparkMax::MotorType::kBrushless};
   rev::CANSparkMax m_frontrightMotor{frontrightMotorDeviceID, rev::CANSparkMax::MotorType::kBrushless};
   rev::CANSparkMax m_backleftMotor{backleftMotorDeviceID, rev::CANSparkMax::MotorType::kBrushless};
@@ -58,7 +71,7 @@ class Robot : public frc::TimedRobot {
   rev::CANEncoder m_frontleftEncoder = m_frontleftMotor.GetEncoder();
   frc::DifferentialDrive m_robotDrive{m_frontleftMotor, m_frontrightMotor};
  
- //hopper and shooter and turret//
+  //hopper and shooter and turret//
   rev::CANSparkMax m_hopperMotor{hopperMotorDeviceID, rev::CANSparkMax::MotorType::kBrushless};
   rev::CANSparkMax m_rightshooterMotor{rightshooterMotorDeviceID, rev::CANSparkMax::MotorType::kBrushless};
   rev::CANSparkMax m_leftshooterMotor{leftshooterMotorDeviceID, rev::CANSparkMax::MotorType::kBrushless};
@@ -67,6 +80,9 @@ class Robot : public frc::TimedRobot {
   rev::CANEncoder m_rightshooterEncoder = m_rightshooterMotor.GetEncoder();
   rev::CANEncoder m_leftshooterEncoder = m_leftshooterMotor.GetEncoder();
   rev::CANEncoder m_turretEncoder = m_turretMotor.GetEncoder();
+
+  rev::CANPIDController m_turretPID = m_turretMotor.GetPIDController();
+  rev::CANPIDController m_shooterPID = m_rightshooterMotor.GetPIDController();
   
   //climber//
   rev::CANSparkMax m_raisingMotor{raisingMotorDeviceID, rev::CANSparkMax::MotorType::kBrushless};
@@ -92,4 +108,24 @@ class Robot : public frc::TimedRobot {
   frc::DoubleSolenoid m_uptake{3, 4};
 
   frc::Compressor m_compressor;
+
+  // Limelight
+  std::shared_ptr<NetworkTable> m_table = nt::NetworkTableInstance::GetDefault().GetTable("limelight-scorpio");
+  double tx_OFFSET = 3.0;
+
+  // PID coefficient structure
+  struct pidCoeff {
+    double kP;
+    double kI;
+    double kD;
+    double kIz;
+    double kFF;
+    double kMinOutput;
+    double kMaxOutput;
+  };
+
+  // DETERMINE THESE EXPERIMENTALLY!!!!!!!
+  // pidCoeff ----------------{kP, kI, kD, kIz, kFF, kMinOutput, kMaxOutput}
+  pidCoeff m_shooterPIDCoeff {0.0005, 0.0, 0.009, 0.0, 0.000199, -1.0, 1.0};
+  pidCoeff m_turretPIDCoeff {0.03, 0.0, 0.0, 0.0, 0.0, -1.0, 1.0};
 };
